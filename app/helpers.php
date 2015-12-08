529bcf54f167def1615b9cc3f62d4b616541bf17
Davide Bellini <bellini.davide@gmail.com>
2015-12-09 00:52:27 +0100

    Aggiunto alert Success

---
+++ app/helpers.php
@@ -0,0 +1,45 @@
+<?php
+
+if (! function_exists('createAuthorsFields')) {
+    /**
+     * Creazione campi autore.
+     *
+     * @return void
+     */
+    function createAuthorsFields(Illuminate\Database\Schema\Blueprint $table)
+    {
+        $table->integer('created_by')->unsigned()->nullable();
+        $table->integer('updated_by')->unsigned()->nullable();
+        $table->integer('deleted_by')->unsigned()->nullable();
+    }
+}
+
+if (! function_exists('removeAuthorsFields')) {
+    /**
+     * Rimozione campi autore.
+     *
+     * @return void
+     */
+    function removeAuthorsFields(Illuminate\Database\Schema\Blueprint $table)
+    {
+        $table->dropColumn('created_by');
+        $table->dropColumn('updated_by');
+        $table->dropColumn('deleted_by');
+    }
+}
+
+if (! function_exists('formDelete')) {
+    /**
+     * Crea la form per la cancellazione di un record.
+     *
+     * @return string
+     */
+    function formDelete($url)
+    {
+        $form = Form::open(['url' => $url, 'method' => 'DELETE', 'class' => 'form-delete']);
+        $form .= '<button type="submit" class="btn btn-xs btn-danger"><span class="glyphicon glyphicon-remove"></span></button>';
+        $form .= Form::close();
+
+        return $form;
+    }
+}
--- resources/views/partials/alert.blade.php
+++ resources/views/partials/alert.blade.php
@@ -9,3 +9,13 @@
     @endforeach
 </div>
 @endif
+
+@if(session()->has('success'))
+<div class="alert alert-success">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+
+    {{ session('success') }}
+</div>
+@endif
